d1951acbcb55f91ad833663d13e68a866057ecb8
AhmadMahmoudTarfaoui <ahmadmahmoudtarfaoui@outlook.com>
2025-05-10 22:04:55 +0100

    refactor HomePage component layout and structure for improved rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+import Image from "next/image";
+import { Canvas } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+import Model from "./_components/Model";
+
 export default function HomePage() {
   return (
-    <main className="flex h-full flex-col items-center justify-center">
-      
+    <main className="flex min-h-[50vh] flex-col items-center justify-center overflow-hidden">
+      <Image
+        src={"/cloudless.png"}
+        alt=""
+        width={4016}
+        height={540}
+        className="absolute top-0 -z-10 h-[50vh] w-[100vw]"
+      />
+      <Canvas style={{ width: "50vw", height: "25vh", position: "absolute", top:200 }}>
+        <ambientLight intensity={0.5} />
+        <Environment 
+        preset="park"
+        />
+        <Model />
+      </Canvas>
     </main>
   );
 }
