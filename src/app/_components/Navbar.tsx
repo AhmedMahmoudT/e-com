9fbb40863c4f35a9e80c6e33db5b9d796edb1a28
AhmadMahmoudTarfaoui <ahmadmahmoudtarfaoui@outlook.com>
2025-05-10 22:05:09 +0100

    Enhance Navbar styling: adjust background opacity and change active link underline color to white for better visibility.

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     const currentPath = usePathname()
   return (
     <div className="flex w-full flex-col items-center justify-center">
-      <div className="flex h-12 w-full items-center justify-center bg-black text-white">
+      <div className="flex h-12 w-full items-center justify-center bg-black/50 text-white">
         <div className="flex w-[60vw] items-center justify-between">
           <p>Free shipping, 30-day return or refund guarantee.</p>
           <div className="flex items-center justify-center gap-10 tracking-[.20rem]">
@@ -16,7 +16,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="flex h-20 w-full items-center justify-center">
+      <div className="flex h-20 w-full items-center justify-center text-white">
         <div className="flex w-[60vw] items-center justify-between">
           <Link href={"/"} className="text-2xl font-bold tracking-widest">
             E-COM
@@ -24,19 +24,19 @@ const Navbar = () => {
           <div className="flex items-center justify-center gap-8 uppercase tracking-widest">
             <Link className={`text-center relative ${currentPath=="/"&&"font-semibold"} `} href={"/"}>
               Home
-              <div className={`absolute -bottom-2 bg-black w-full h-[2px] ${currentPath!=="/"&&"hidden"}`}></div>
+              <div className={`absolute -bottom-2 bg-white w-full h-[2px] ${currentPath!=="/"&&"hidden"}`}></div>
             </Link>
             <Link className={`text-center relative ${currentPath=="/shop"&&"font-semibold"} `} href={"/shop"}>
               Shop
-              <div className={`absolute -bottom-2 bg-black w-full h-[2px] ${currentPath!=="/shop"&&"hidden"}`}></div>
+              <div className={`absolute -bottom-2 bg-white w-full h-[2px] ${currentPath!=="/shop"&&"hidden"}`}></div>
             </Link>
             <Link className={`text-center relative ${currentPath=="/deals"&&"font-semibold"} `} href={"/deals"}>
               Deals
-              <div className={`absolute -bottom-2 bg-black w-full h-[2px] ${currentPath!=="/deals"&&"hidden"}`}></div>
+              <div className={`absolute -bottom-2 bg-white w-full h-[2px] ${currentPath!=="/deals"&&"hidden"}`}></div>
             </Link>
             <Link className={`text-center relative ${currentPath=="/contact"&&"font-semibold"} `} href={"/contact"}>
               Contact
-              <div className={`absolute -bottom-2 bg-black w-full h-[2px] ${currentPath!=="/contact"&&"hidden"}`}></div>
+              <div className={`absolute -bottom-2 bg-white w-full h-[2px] ${currentPath!=="/contact"&&"hidden"}`}></div>
             </Link>
           </div>
           <div className="flex items-center justify-between gap-8 text-2xl">
