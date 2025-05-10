7707e7c8e9495f46e8bf8da24e1ecd27876b57ce
AhmadMahmoudTarfaoui <ahmadmahmoudtarfaoui@outlook.com>
2025-05-10 22:04:37 +0100

    add Model component with animated sphere and text

---
+++ src/app/_components/Model.tsx
@@ -0,0 +1,100 @@
+import { useFrame } from "@react-three/fiber";
+import { Text } from "@react-three/drei";
+import { useRef, useState, type MutableRefObject } from "react";
+import type * as THREE from "three";
+
+const Model = () => {
+  const mesh = useRef<THREE.Mesh>(null!);
+  const [x, setX] = useState(1);
+  const [y, setY] = useState(1);
+  const [ascending, setAscending] = useState(true);
+  useFrame(() => {
+    if (x >= 10) {
+      setAscending(false);
+    }
+    if (x <= 1) {
+      setAscending(true);
+    }
+    mesh.current.rotation.x += 0.02;
+    mesh.current.rotation.y += 0.02;
+
+    if (ascending) {
+      setX((prev) => prev + 0.025);
+      setY((prev) => prev + 0.025);
+    } else {
+      setX((prev) => prev - 0.025);
+      setY((prev) => prev - 0.025);
+    }
+  });
+  return (
+    <group scale={0.9}>
+      <Text
+        position={[0, 0, -5]}
+        fontSize={4}
+        font="fonts/DelaGothicOne-Regular.ttf"
+      >
+        Discover your Taste
+      </Text>
+
+      <Sphere
+        args={[3, x, y]}
+        color="#008BDE"
+        opacity={0.5}
+        transparent={true}
+        mesh={mesh}
+      />
+
+      {/* <Cube
+        args={[3, 3, 3]}
+        color="purple"
+        opacity={.75}
+        transparent={true}
+        mesh={mesh}
+      /> */}
+
+    </group>
+  );
+};
+
+type GeometryTypes = {
+  args: [
+    width?: number,
+    height?: number,
+    depth?: number,
+    widthSegments?: number,
+    heightSegments?: number,
+    depthSegments?: number,
+  ];
+  color: string;
+  opacity: number;
+  transparent: boolean;
+  mesh: MutableRefObject<THREE.Mesh>;
+};
+
+const Cube = ({ args, color, opacity, transparent, mesh }: GeometryTypes) => {
+  return (
+    <mesh ref={mesh}>
+      <boxGeometry args={args} />
+      <meshStandardMaterial
+        color={color}
+        opacity={opacity}
+        transparent={transparent}
+      />
+    </mesh>
+  );
+};
+
+const Sphere = ({ args, color, opacity, transparent, mesh }: GeometryTypes) => {
+  return (
+    <mesh ref={mesh}>
+      <sphereGeometry args={args} />
+      <meshStandardMaterial
+        color={color}
+        opacity={opacity}
+        transparent={transparent}
+      />
+    </mesh>
+  );
+};
+
+export default Model;
