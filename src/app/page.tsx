a307819441eeb0e575d34664eb2e0fe598439a64
AhmadMahmoudTarfaoui <ahmadmahmoudtarfaoui@outlook.com>
2025-06-01 00:35:49 +0100

    Refactor HomePage component for improved layout and responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,22 @@ import Model from "./_components/Model";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-[500px] flex-col items-center justify-center w-screen">
+    <main className="flex min-h-screen flex-col items-center justify-center overflow-hidden">
       <Image
         src={"/cloudless.png"}
         alt=""
         width={4016}
         height={540}
-        className="absolute top-0 -z-10 h-[500px] w-screen"
+        className="absolute top-0 -z-10 h-screen w-full"
       />
-      <Canvas style={{ width: "100vw", height: "250px", position: "absolute", top:200 }}>
+      <Canvas style={{ width: "100vw", height: "60vh", marginBottom: "10rem" }}>
         <ambientLight intensity={0.5} />
         <Environment 
         preset="park"
         />
         <Model />
       </Canvas>
+      <p>Hello</p>
     </main>
   );
 }
